67e1c92cae9f7c4c484638305b4798ec6e86bf92
lakhankumar <lakhankumar997@gmail.com>
2021-07-27 02:07:28 +0530

    Added: multiple flip coin

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,9 +1,18 @@
 #!/bin/bash -x
-
+read -p "Enter how many times you want to flip coin: " num
+head=0
+tail=0
+for((i=1; i<=$num; i++))
+do
 toss=$(($RANDOM%2))
 if [ $toss -eq 1 ]
 then
 	echo "Its head"
+	head=$(($head+1))
 else
 	echo "Its tail"
+	tail=$(($tail+1))
 fi
+done
+echo "no.of head= $head"
+echo "no.of tail= $tail"
